169bc5094e82b65407de397923361a5405ef6214
DracoBlue <JanS@DracoBlue.de>
2011-01-04 21:25:09 +0100

    * fixed rows for doctrine queries * added support for named parameters * limited the debug backtrace to 6 elements for the doctrine queries

--- lib/agavi/PhpDebugToolbarDoctrineConnectionListener.class.php
+++ lib/agavi/PhpDebugToolbarDoctrineConnectionListener.class.php
@@ -14,16 +14,25 @@ class PhpDebugToolbarDoctrineConnectionListener extends Doctrine_EventListener {
         $query_string = $query_group;
         $params = $event->getParams();
 
-        if ($params) {
-            foreach ($params as $value) {
+        if ($params)
+        {
+            foreach ($params as $key => $value)
+            {
                 /*
                  * Just replace the first ?, not all!
                  */
-                $query_string = preg_replace('/\?/', $value, $query_string, 1);
+                if (is_numeric($key))
+                {
+                    $query_string = preg_replace('/\?/', $value, $query_string, 1);
+                }
+                else
+                {
+                    $query_string = str_replace(':'.$key, is_numeric($value) ? $value : "'" . $value . "'", $query_string);
+                }
             }
         }
 
-        $backtrace = array_slice(debug_backtrace(false), 5);
+        $backtrace = array_slice(debug_backtrace(false), 5, 6);
         foreach ($backtrace as $k => $v)
         {
             unset($backtrace[$k]['args']);
@@ -38,12 +47,19 @@ class PhpDebugToolbarDoctrineConnectionListener extends Doctrine_EventListener {
         $this->last_exec_start_memory = memory_get_usage();       
     }
 
-    protected function appendValueForLastExecInfo()
+    protected function appendValueForLastExecInfo(Doctrine_Event $event)
     {
+        $time = (microtime(true) - $this->last_exec_start_time);
         $memory = (memory_get_usage() - $this->last_exec_start_memory);
         $this->last_exec_info['memory'] = $memory;
-        $time = (microtime(true) - $this->last_exec_start_time);
         $this->last_exec_info['time'] = $time;
+        $invoker = $event->getInvoker();
+        $row_count = 0;
+        if (is_callable(array($invoker, 'rowCount')))
+        {
+            $row_count = $invoker->rowCount();
+        }
+        $this->last_exec_info['rows'] = $row_count;
         PhpDebugToolbar::appendValue('database_queries', $this->last_exec_info);
         DoctrineDatabaseToolbarExtension::$count++;
         DoctrineDatabaseToolbarExtension::$time += $time;
@@ -56,7 +72,7 @@ class PhpDebugToolbarDoctrineConnectionListener extends Doctrine_EventListener {
 
     public function postExec(Doctrine_Event $event)
     {
-        $this->appendValueForLastExecInfo();
+        $this->appendValueForLastExecInfo($event);
     }
 
     public function preStmtExecute(Doctrine_Event $event)
@@ -66,7 +82,7 @@ class PhpDebugToolbarDoctrineConnectionListener extends Doctrine_EventListener {
 
     public function postStmtExecute(Doctrine_Event $event)
     {
-        $this->appendValueForLastExecInfo();
+        $this->appendValueForLastExecInfo($event);
     }
  
     public function preQuery(Doctrine_Event $event)
@@ -76,6 +92,6 @@ class PhpDebugToolbarDoctrineConnectionListener extends Doctrine_EventListener {
 
     public function postQuery(Doctrine_Event $event)
     {
-        $this->appendValueForLastExecInfo();
+        $this->appendValueForLastExecInfo($event);
     }
 }
